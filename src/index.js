730ef24cea8bd8f38f796aae6df90b9f35c45eb9
vovoSoul <164639325+vovoSoul@users.noreply.github.com>
2024-04-21 16:39:45 -0400

    added functionality to search engine using API
    -made a default city, to display accurate data
    -multiple functions added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
+function updateWeather(response) {
+  console.log(response.data.temperature.current);
+  let temperature = document.querySelector("#temp-figure");
+  let temperaturefigure = Math.round(response.data.temperature.current);
+  temperature.innerHTML = temperaturefigure;
+
+  let cityprint = document.querySelector("#city");
+  cityprint.innerHTML = response.data.city;
+}
+
+function searchCityweather(city) {
+  let api_key = "ee88953fd2a466oa4c850t79d8cb473f";
+  let api_url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${api_key}`;
+  console.log(api_url);
+  axios.get(api_url).then(updateWeather);
+}
+
 function citySearch(event) {
   event.preventDefault();
   let searchbox = document.querySelector("#search-input");
   /*console.log(searchbox.value);*/
-  let cityprint = document.querySelector("#city");
-  cityprint.innerHTML = searchbox.value;
+
+  searchCityweather(searchbox.value);
 }
 
 let searchform = document.querySelector("#search-forms");
 searchform.addEventListener("click", citySearch);
+
+searchCityweather("New york");
