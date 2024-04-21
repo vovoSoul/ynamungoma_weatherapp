d3879c61162c639f1bad2995e786d7f92ef31b06
vovoSoul <164639325+vovoSoul@users.noreply.github.com>
2024-04-21 13:59:47 -0400

    added basic JS to search engine

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function citySearch(event) {
+  event.preventDefault();
+  let searchbox = document.querySelector("#search-input");
+  /*console.log(searchbox.value);*/
+  let cityprint = document.querySelector("#city");
+  cityprint.innerHTML = searchbox.value;
+}
+
+let searchform = document.querySelector("#search-forms");
+searchform.addEventListener("submit", citySearch);
